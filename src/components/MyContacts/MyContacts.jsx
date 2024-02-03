664b8b51c2693e75507d0d6c0afd7193429815d3
sashasashkina <koshyl.apps@gmail.com>
2024-02-03 19:26:44 +0200

    committree

--- src/components/MyContacts/MyContacts.jsx
+++ src/components/MyContacts/MyContacts.jsx
@@ -1,135 +1,13 @@
-// import { useEffect, useState } from 'react';
-// import css from './MyContacts.module.css';
-// import { nanoid } from 'nanoid';
-// import { ContactList } from 'components/ContactList/ContactList';
-// import Phonebook from 'components/Phonebook/Phonebook';
-
-// const MyContacts = () => {
-//   const [contacts, setContacts] = useState(
-//     () => JSON.parse(localStorage.getItem('my-contacts')) || []
-//   );
-//   const [filter, setFilter] = useState('');
-
-//   useEffect(() => {
-//     localStorage.setItem('my-contacts', JSON.stringify(contacts));
-//   }, [contacts]);
-
-//   const isDuplicate = ({ name, number }) => {
-//     const normalizedName = name.toLowerCase();
-//     const normalizedNumber = number.toLowerCase();
-
-//     const duplicate = contacts.find(item => {
-//       const normalizedCurrentName = item.name.toLowerCase();
-//       const normalizedCurrentNumber = item.number.toLowerCase();
-//       return (
-//         normalizedCurrentName === normalizedName &&
-//         normalizedCurrentNumber === normalizedNumber
-//       );
-//     });
-//     return Boolean(duplicate);
-//   };
-
-//   const addContact = data => {
-//     if (isDuplicate(data)) {
-//       return alert(`${data.name} is already in contacts.`);
-//     }
-
-//     setContacts(prevState => [
-//       ...prevState,
-//       {
-//         id: nanoid(),
-//         ...data,
-//       },
-//     ]);
-//   };
-
-//   const deleteContact = id => {
-//     setContacts(prevStates => prevStates.filter(item => item.id !== id));
-//   };
-
-//   const changeFilter = ({ target }) => {
-//     setFilter(target.value);
-//   };
-
-//   const getFilterContacts = () => {
-//     const normalizedFilter = filter.toLocaleLowerCase();
-
-//     const filteredContacts = contacts.filter(({ name, number }) => {
-//       const normalizedName = name.toLocaleLowerCase();
-//       const normalizedNumber = number.toLocaleLowerCase();
-//       return (
-//         normalizedName.includes(normalizedFilter) ||
-//         normalizedNumber.includes(normalizedFilter)
-//       );
-//     });
-
-//     return filteredContacts;
-//   };
-
-//   const filteredContacts = getFilterContacts();
-
-//   return (
-//     <div className={css.wrapper}>
-//       <h2 className={css.title}>Phonebook</h2>
-//       <Phonebook onSubmit={addContact} />
-//       <h2 className={css.title}>Contacts</h2>
-//       <div className={css.listWrapper}>
-//         <input
-//           onChange={changeFilter}
-//           name="filter"
-//           placeholder="Find contacts by name"
-//         />
-//         <ContactList
-//           contacts={filteredContacts}
-//           deleteContact={deleteContact}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MyContacts;
-
 import css from './MyContacts.module.css';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { ContactList } from 'components/ContactList/ContactList';
 import Phonebook from 'components/Phonebook/Phonebook';
-import { addContact, deleteContact } from '../../redux/Contacts/contacts-slice';
+
 import { setFilter } from '../../redux/Filter/filter-slice';
-import { getFilteredContacts } from '../../redux/Contacts/contacts-selectors';
 
 const MyContacts = () => {
-  const contacts = useSelector(getFilteredContacts);
-
   const dispatch = useDispatch();
-
-  const isDuplicate = ({ name, number }) => {
-    const normalizedName = name.toLowerCase();
-    const normalizedNumber = number.toLowerCase();
-
-    const duplicate = contacts.find(item => {
-      const normalizedCurrentName = item.name.toLowerCase();
-      const normalizedCurrentNumber = item.number.toLowerCase();
-      return (
-        normalizedCurrentName === normalizedName &&
-        normalizedCurrentNumber === normalizedNumber
-      );
-    });
-    return Boolean(duplicate);
-  };
-
-  const onAddContact = data => {
-    if (isDuplicate(data)) {
-      return alert(`${data.name} is already in contacts.`);
-    }
-    const action = addContact(data);
-    dispatch(action);
-  };
-
-  const onDeleteContact = id => {
-    dispatch(deleteContact(id));
-  };
 
   const changeFilter = ({ target }) => {
     dispatch(setFilter(target.value));
@@ -138,7 +16,7 @@ const MyContacts = () => {
   return (
     <div className={css.wrapper}>
       <h2 className={css.title}>Phonebook</h2>
-      <Phonebook onSubmit={onAddContact} />
+      <Phonebook />
       <h2 className={css.title}>Contacts</h2>
       <div className={css.listWrapper}>
         <input
@@ -146,7 +24,7 @@ const MyContacts = () => {
           onChange={changeFilter}
           placeholder="Find contacts by name"
         />
-        <ContactList contacts={contacts} deleteContact={onDeleteContact} />
+        <ContactList />
       </div>
     </div>
   );
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,21 +1,44 @@
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import css from './Phonebook.module.css';
-//  import { useDispatch, useSelector } from 'react-redux';
-// import { addContact } from '../../redux/Contacts/contacts-slice';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from '../../redux/Contacts/contacts-slice';
+import { getFilteredContacts } from '../../redux/Contacts/contacts-selectors';
 
 const INITIAL_STATE = {
   name: '',
   number: '',
 };
 
-const Phonebook = ({ onSubmit }) => {
-  // const dispatch = useDispatch();
+const Phonebook = () => {
+  const dispatch = useDispatch();
 
-  // const { contacts } = useSelector(getFilteredContacts);
+  const { contacts } = useSelector(getFilteredContacts);
   const nameId = nanoid();
   const numberId = nanoid();
   const [value, setValue] = useState({ ...INITIAL_STATE });
+
+  const isDuplicate = ({ name, number }) => {
+    const normalizedName = name.toLowerCase();
+    const normalizedNumber = number.toLowerCase();
+
+    const duplicate = contacts.find(item => {
+      const normalizedCurrentName = item.name.toLowerCase();
+      const normalizedCurrentNumber = item.number.toLowerCase();
+      return (
+        normalizedCurrentName === normalizedName &&
+        normalizedCurrentNumber === normalizedNumber
+      );
+    });
+    return Boolean(duplicate);
+  };
+  const onAddContact = data => {
+    if (isDuplicate(data)) {
+      return alert(`${data.name} is already in contacts.`);
+    }
+    const action = addContact(data);
+    dispatch(action);
+  };
 
   const handleChange = ({ target }) => {
     const { name, value } = target;
@@ -24,7 +47,7 @@ const Phonebook = ({ onSubmit }) => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    onSubmit(value);
+    onAddContact(value);
     reset();
   };
 
@@ -70,76 +93,3 @@ const Phonebook = ({ onSubmit }) => {
 };
 
 export default Phonebook;
-
-// import { useState } from 'react';
-// import { nanoid } from 'nanoid';
-// import css from './Phonebook.module.css';
-
-// const Phonebook = ({ onSubmit }) => {
-//   const [name, setName] = useState('');
-//   const [number, setNumber] = useState('');
-
-//   const nameId = nanoid();
-//   const numberId = nanoid();
-
-//   const handleChange = event => {
-//     const { name, value } = event.currentTarget;
-//     switch (name) {
-//       case 'name':
-//         setName(value);
-//         break;
-//       case 'number':
-//         setNumber(value);
-//         break;
-//       default:
-//         return;
-//     }
-//   };
-
-//   const handleSubmit = event => {
-//     event.preventDefault();
-//     onSubmit({ name, number });
-//     setName('');
-//     setNumber('');
-//   };
-
-//   return (
-//     <form className={css.form} onSubmit={handleSubmit}>
-//       <div className={css.formGroup}>
-//         <label htmlFor={nameId}>Name</label>
-//         <input
-//           className={css.input}
-//           id={nameId}
-//           placeholder="Name contacts"
-//           name="name"
-//           type="text"
-//           required
-//           value={name}
-//           // pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//           onChange={handleChange}
-//         />
-//       </div>
-//       <div className={css.formGroup}>
-//         <label className={css.title} htmlFor={numberId}>
-//           Number
-//         </label>
-//         <input
-//           className={css.input}
-//           id={numberId}
-//           placeholder="Number contacts"
-//           type="text"
-//           name="number"
-//           required
-//           value={number}
-//           // pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-//           onChange={handleChange}
-//         />
-//       </div>
-//       <button className={css.button} type="submit">
-//         Add contact
-//       </button>
-//     </form>
-//   );
-// };
-
-// export default Phonebook;
