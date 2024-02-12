62ac48f5b8ed89e90a81d7997310fdb56ff6f101
sashasashkina <koshyl.apps@gmail.com>
2024-02-12 20:19:49 +0200

    commitseven

--- src/redux/Contacts/contacts-selectors.js
+++ src/redux/Contacts/contacts-selectors.js
@@ -1,15 +1,35 @@
+//  export const selectAllContacts = store => store.contacts;
+
+// export const selectFilteredContacts = store => {
+//   const { contacts, filter } = store;
+
+//   const normalizedFilter = filter.toLocaleLowerCase();
+
+//   const filteredContacts = contacts.items.filter(({ name }) => {
+//     const normalizedName = name;
+
+//     return normalizedName.includes(normalizedFilter);
+//   });
+
+//   return filteredContacts;
+// };
+import { createSelector } from '@reduxjs/toolkit';
 export const selectAllContacts = store => store.contacts;
 
-export const selectFilteredContacts = store => {
-  const { contacts, filter } = store;
+export const selectFilter = store => store.filter;
+export const selectError = state => state.contacts.error;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectContacts = store => store.contacts.items;
 
-  const normalizedFilter = filter.toLocaleLowerCase();
-
-  const filteredContacts = contacts.items.filter(({ name }) => {
-    const normalizedName = name;
-
-    return normalizedName.includes(normalizedFilter);
-  });
-
-  return filteredContacts;
-};
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    if (!filter) {
+      return contacts;
+    }
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
