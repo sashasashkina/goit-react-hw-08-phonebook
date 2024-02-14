3214443860ef972537c9b3b2bb8a8965aa40527f
sashasashkina <138327048+sashasashkina@users.noreply.github.com>
2024-02-14 17:51:51 +0000

    Deploying to gh-pages from @ sashasashkina/goit-react-hw-08-phonebook@d64041ca3d43d2517a86f5e22f25c275d605dc7b 🚀

---
+++ static/js/156.b5cd7958.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[156],{2156:function(e,t,n){n.r(t);var r=n(1087),a=n(184);t.default=function(){return(0,a.jsxs)("div",{children:[(0,a.jsx)("h1",{children:"Cannot found this page"}),(0,a.jsx)(r.rU,{to:"/",children:"To home page"})]})}}}]);
+//# sourceMappingURL=156.b5cd7958.chunk.js.map
---
+++ static/js/795.bb4a887d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[795],{1795:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h1",{children:"Home page"})})}}}]);
+//# sourceMappingURL=795.bb4a887d.chunk.js.map
